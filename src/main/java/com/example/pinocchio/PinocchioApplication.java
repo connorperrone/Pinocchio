9dbb70ccfc1dbc2e8df0e605b46df78668ce18c7
Connor <connorperrone@gmail.com>
2023-01-10 10:58:29 -0800

    Minimize and exit buttons

--- src/main/java/com/example/pinocchio/PinocchioApplication.java
+++ src/main/java/com/example/pinocchio/PinocchioApplication.java
@@ -4,9 +4,12 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -25,13 +28,48 @@ public class PinocchioApplication extends Application {
 
 
 
-        HBox topBarHBox = new HBox();
-        topBarHBox.setMinHeight(30);
-        topBarHBox.setBackground(new Background(new BackgroundFill(Color.valueOf("0xADC178"), CornerRadii.EMPTY, Insets.EMPTY)));
+        BorderPane topBarBorderPane = new BorderPane();
+        topBarBorderPane.setMinHeight(32);
+        topBarBorderPane.setMaxHeight(32);
+        topBarBorderPane.setBackground(new Background(new BackgroundFill(Color.valueOf("0xADC178"), CornerRadii.EMPTY, Insets.EMPTY)));
+
         Label label = new Label("Pinocchio");
-        topBarHBox.getChildren().add(label);
-        topBarHBox.setAlignment(Pos.BOTTOM_LEFT);
-        vBox.getChildren().add(topBarHBox);
+        label.setMaxHeight(32);
+        label.setFont(new Font(18));
+        label.setPadding(new Insets(0, 0, 0, 8));
+        topBarBorderPane.setLeft(label);
+
+        ImageView minimizeButtonImageView = new ImageView("minimize_icon.png");
+        minimizeButtonImageView.setPreserveRatio(true);
+        minimizeButtonImageView.setFitWidth(24);
+        minimizeButtonImageView.setFitHeight(24);
+
+        Button minimizeButton = new Button();
+        minimizeButton.setGraphic(minimizeButtonImageView);
+        minimizeButton.setMinSize(24, 32);
+        minimizeButton.setMaxSize(24, 32);
+        minimizeButton.setBackground(Background.EMPTY);
+        minimizeButton.setOnAction(event -> stage.setIconified(true));
+
+        ImageView exitButtonImageView = new ImageView("exit_icon.png");
+        exitButtonImageView.setPreserveRatio(true);
+        exitButtonImageView.setFitWidth(24);
+        exitButtonImageView.setFitHeight(24);
+
+        Button exitButton = new Button();
+        exitButton.setGraphic(exitButtonImageView);
+        exitButton.setMinSize(24, 32);
+        exitButton.setMaxSize(24, 32);
+        exitButton.setBackground(Background.EMPTY);
+        exitButton.setOnAction(event -> System.exit(0));
+
+        HBox buttonsHBox = new HBox();
+        buttonsHBox.getChildren().addAll(minimizeButton, exitButton);
+        buttonsHBox.setPadding(new Insets(0, 8, 0, 0));
+        buttonsHBox.setSpacing(8);
+        topBarBorderPane.setRight(buttonsHBox);
+
+        vBox.getChildren().add(topBarBorderPane);
 
 
 
